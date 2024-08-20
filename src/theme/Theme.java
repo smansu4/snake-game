f396d4b2ce7966da975969479f643e526f414dd4
Smansu4 <15002688+smansu4@users.noreply.github.com>
2024-08-19 21:01:47 -0500

    make themes inner class

--- src/theme/Theme.java
+++ src/theme/Theme.java
@@ -1,5 +1,6 @@
 package theme;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +8,7 @@ public class Theme {
 
     private static Theme theme;
 
-    private List<ColorPalette> themesList;
+    private final List<ColorPalette> themesList;
     private boolean useDefaultTheme = true;
 
     private Theme() {
@@ -34,5 +35,57 @@ public class Theme {
             return themesList.get(0);
         }
         return themesList.get(1);
+    }
+
+    private static final class ClassicTheme implements ColorPalette {
+        @Override
+        public Color getSnakeColor() {
+            return Color.GREEN;
+        }
+
+        @Override
+        public Color getFoodColor() {
+            return Color.RED;
+        }
+
+        @Override
+        public Color getTextColor() {
+            return Color.RED;
+        }
+
+        @Override
+        public Color getScoreColor() {
+            return Color.WHITE;
+        }
+    }
+
+    /**
+     * The color palette of this theme was selected from the Okabe and Ito palette.
+     * Palette can be found here: <a href="https://siegal.bio.nyu.edu/color-palette/">...</a>
+     * <p>
+     * There are many color-blind friendly palettes available online.
+     * This theme was chosen because it also contains black and I prefer black for
+     * the background.
+     */
+    private static final class OkabeItoTheme implements ColorPalette {
+        @Override
+        public Color getSnakeColor() {
+            return new Color(240, 228, 66);
+        }
+
+        @Override
+        public Color getFoodColor() {
+            return new Color(0, 114, 178);
+        }
+
+        @Override
+        public Color getTextColor() {
+            return new Color(213, 94, 0);
+        }
+
+        @Override
+        public Color getScoreColor() {
+            return new Color(0,158,115);
+        }
     }
 }
