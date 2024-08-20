c36149cc0d63070b6f327545c99f85778b604760
Smansu4 <15002688+smansu4@users.noreply.github.com>
2024-08-19 23:05:29 -0500

    refactor snake

---
+++ src/Snake.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snake {
+    public final Tile head;
+    public final List<Tile> body;
+
+    int velocityX;
+    int velocityY;
+
+    public Snake() {
+        head = new Tile(5,5);
+        body = new ArrayList<>();
+
+        velocityX = 0;
+        velocityY = 0;
+    }
+
+    public void move() {
+        // The snake body does not know where to go,
+        // as it is following the lead of the head.
+        // The body tiles need to catch up to the tile
+        // before it;
+        for(int i = body.size() - 1; i >= 0; i--) {
+            Tile snakeSegment = body.get(i);
+
+            //This is the first body part right before head
+            if(i == 0) {
+                snakeSegment.setX(head.getX());
+                snakeSegment.setY(head.getY());
+            } else {
+                Tile prevSnakeSegment = body.get(i - 1);
+                snakeSegment.setX(prevSnakeSegment.getX());
+                snakeSegment.setY(prevSnakeSegment.getY());
+            }
+        }
+
+        //Snake Head
+        head.x += velocityX;
+        head.y += velocityY;
+    }
+
+    public void stop() {
+        velocityX = 0;
+        velocityY = 0;
+    }
+
+    public void eat(Tile tile) {
+        body.add(tile);
+    }
+
+    public void shedBody() {
+        body.clear();
+    }
+
+    public void resetSnake() {
+        shedBody();
+
+        velocityX = 0;
+        velocityY = 0;
+    }
+}
---
+++ src/Tile.java
@@ -0,0 +1,29 @@
+
+
+public class Tile {
+    public static final int SIZE = 25;
+
+    int x;
+    int y;
+
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
